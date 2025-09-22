d5e499bb680dadefc1d153dc5927c760222d1bce
zabronax <zabronax@gmail.com>
2025-09-22 12:42:38 +0200

    setup crud endpoints

--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/hello", () => "Hello World!");
-app.MapGet("/bye", () => "Goodbye!");
+app.MapPost("/", (string body) =>
+{
+  return "Was called with Method: POST; Path: /";
+});
+
+app.MapGet("/", () =>
+{
+  return "Was called with Method: GET; Path: /";
+});
+
+app.MapPut("/", () =>
+{
+  return "Was called with Method: PUT; Path: /";
+});
+
+app.MapDelete("/", () =>
+{
+  return "Was called with Method: DELETE; Path: /";
+});
 
 app.Run();
