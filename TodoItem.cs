b01f3e29d87ed8eabfc4134a103938ebe605fba9
zabronax <zabronax@gmail.com>
2025-09-23 10:48:01 +0200

    add more complex todo creation logic

--- TodoItem.cs
+++ TodoItem.cs
@@ -1,3 +1,5 @@
+using System.IO.Compression;
+
 class TodoItem
 {
   // Properties / Egenskaper
@@ -11,6 +13,14 @@ class TodoItem
   {
     IsComplete = false;
     CreatedAt = DateTime.Now;
+
+    // We can setup rules for what's allowed here
+    if (description.Length < 3 && description.Length < 200)
+    {
+      // This crashes this part of the program if we use an invalid description
+      throw new ArgumentException();
+    }
+
     Description = description;
     Deadline = deadline;
   }
