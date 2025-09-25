1dae47d61a63821771feef4c521d6eabac886769
zabronax <zabronax@gmail.com>
2025-09-25 14:57:26 +0200

    hook up form

--- wwwroot/main.js
+++ wwwroot/main.js
@@ -1,5 +1,30 @@
 console.log("JavaScript loaded")
 
+// Setup form logic
+const todoForm = document.querySelector("#todo-input-form")
+todoForm.addEventListener("submit", (event) => {
+  event.preventDefault() // Don't refresh
+  
+  // Read off current values from inputs
+  const descriptionElement = todoForm.querySelector("#description")
+  const description = descriptionElement.value
+
+  // Package into a nice object
+  const formPayload = {
+    Description: description,
+  }
+  console.log(formPayload)
+
+  // Send to server
+  fetch("http://localhost:5282/todo", {
+    method: "POST",
+    body: JSON.stringify(formPayload),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+})
+
 // Wait for the first HTTP response
 const response = await fetch("http://localhost:5282/todo")
 console.log(response)
