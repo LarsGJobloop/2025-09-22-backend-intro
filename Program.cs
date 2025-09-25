220b7de513c52ee238e528db929bf7ed09ee0bae
zabronax <zabronax@gmail.com>
2025-09-25 10:30:52 +0200

    extract TodoList to service

--- Program.cs
+++ Program.cs
@@ -1,23 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<TodoList>();
+
 var app = builder.Build();
 
-var todoList = new TodoList();
+// Router definitions (Mappings)
 
 // Read all todoes
-app.MapGet("/todo", () =>
+app.MapGet("/todo", (TodoList todoList) =>
 {
   return todoList.GetAllTodoes();
 });
 
 // Create a new todo
-app.MapPost("/todo", (TodoItemCreateInfo createInfo) =>
+app.MapPost("/todo", (TodoList todoList, TodoItemCreateInfo createInfo) =>
 {
   var newTodoItem = todoList.CreateNewTodo(createInfo);
 
   return newTodoItem;
 });
 
-app.MapDelete("/todo/{todoId}", (string todoId) =>
+app.MapDelete("/todo/{todoId}", (TodoList todoList, string todoId) =>
 {
   todoList.DeleteTodo(todoId);
 });
