9e4fdcfb1220785c0f7fc66725e76336597bc8df
zabronax <zabronax@gmail.com>
2025-09-25 14:38:28 +0200

    render todo items

--- wwwroot/main.js
+++ wwwroot/main.js
@@ -1,1 +1,18 @@
 console.log("JavaScript loaded")
+
+// Wait for the first HTTP response
+const response = await fetch("http://localhost:5282/todo")
+console.log(response)
+
+// Wait for all data and unwrap
+const todoList = await response.json()
+console.log(todoList)
+
+for (const todoItem of todoList) {
+  console.log(todoItem)
+  const todoElement = document.createElement("li")
+  todoElement.textContent = todoItem.description
+  
+  const todoListElement = document.querySelector("#todo-list")
+  todoListElement.append(todoElement)
+}
